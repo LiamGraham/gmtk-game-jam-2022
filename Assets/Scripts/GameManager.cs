3e0a8bfd99d8e7438e1b319d58946cf7f54b144c
Alexander Blyth <alex@3cssoftware.com.au>
2022-07-16 20:31:16 +1000

    Add goal event triggering

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public partial class GameManager : MonoBehaviour, IDisposable
+{
+    private GameState state;
+
+    private void Start()
+    {
+        state = GameState.Starting;
+        LevelEventManager.GoalAchieved?.AddListener(GoalReached);
+    }
+
+    void GoalReached()
+    {
+        state = GameState.ObjectiveAchieved;
+        for (int i = 0; i < 10; i++)
+        {
+            Debug.Log("Goal Reached!");
+        }
+    }
+
+    public void Dispose()
+    {
+        LevelEventManager.GoalAchieved?.RemoveListener(GoalReached);
+    }
+}
---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum GameState
+{
+    /// <summary>
+    /// For an overview flyby of the game map
+    /// </summary>
+    Starting, 
+
+    /// <summary>
+    /// When the player has control 
+    /// </summary>
+    Playing,
+
+    /// <summary>
+    /// When the main objective of the map has been reached!
+    /// </summary>
+    ObjectiveAchieved
+}
