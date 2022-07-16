ad5910800cfc3b7a422f048a33616f5b1b52ed41
Alexander Blyth <alex@3cssoftware.com.au>
2022-07-16 17:26:33 +1000

    Update shot indicator to set alpha

--- Assets/Scripts/ShotIndicator.cs
+++ Assets/Scripts/ShotIndicator.cs
@@ -13,40 +13,37 @@ public class ShotIndicator : MonoBehaviour
     public GameObject player;
     SpriteRenderer spriteRenderer;
 
-    // // float currentLength;
-    // // float currentAlpha;
     // // Start is called before the first frame update
     void Start()
     {
-        this.transform.rotation = Quaternion.LookRotation(-Vector3.up, Vector3.forward);
-        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        transform.rotation = Quaternion.LookRotation(-Vector3.up, Vector3.forward);
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // // Update is called once per frame
     void Update()
     {
-        this.setDirection(player.transform.position - this.transform.position);
-        this.setPower((player.transform.position - this.transform.position).magnitude/6f);
+        setDirection(player.transform.position - transform.position);
+        setPower((player.transform.position - transform.position).magnitude/6f);
     }
 
     public void setPower(float power) {
-        //set power as a float from 0 to 1
-        Debug.Log(power.ToString());
-        power = Mathf.Min(0, power);
-        power = Mathf.Max(1, power);
+        //set power bounds from 0 to 1
+        power = Mathf.Max(0, power);
+        power = Mathf.Min(1, power);
+
         //set length
         var newLength = minLength + (maxLength - minLength) * power;
-        this.transform.localScale = (new Vector3(1, newLength, 1)) * this.initialScale;
+        transform.localScale = (new Vector3(1, newLength, 1)) * initialScale;
         //set alpha
         var newAlpha = (minAlpha + (maxAlpha - minAlpha) * power);
-        var newColor = new Color(this.spriteRenderer.color.r, this.spriteRenderer.color.g, this.spriteRenderer.color.b, newAlpha);
-        this.spriteRenderer.color = newColor;
+        var newColor = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, newAlpha);
+        spriteRenderer.color = newColor;
     }
 
     public void setDirection(Vector3 direction) {
-        // this.transform.LookAt(target, Vector3.up);
         var goalRotation = Quaternion.LookRotation(direction, Vector3.up);
         var additionalRotation = Quaternion.LookRotation(-Vector3.up, Vector3.forward);
-        this.transform.rotation = goalRotation * additionalRotation;
+        transform.rotation = goalRotation * additionalRotation;
     }
 }
