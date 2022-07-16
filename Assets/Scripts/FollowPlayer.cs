24d0971973ad5431e8eaf9aeaac73b27069b6f37
Alexander Blyth <alex@3cssoftware.com.au>
2022-07-16 16:27:44 +1000

    Add ZoomCameraIn script

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public GameObject m_Player;
-    public Vector3 m_OffsetY = new Vector3(0, 0.5f, 0);
-    public float m_MinDistance = 3f;
-    public float m_InterpolationSpeed = 2f;
-    public bool m_EnableSmooth = true;
+    public GameObject Player;
+    public Vector3 Offset = new Vector3(0, 0.5f, 0);
+    public float MinDistance = 3f;
+    public float InterpolationSpeed = 2f;
+    public bool EnableSmooth = true;
 
     void LateUpdate()
     {
@@ -18,21 +18,21 @@ public class FollowPlayer : MonoBehaviour
 
     private void PositionFollowCamera()
     {
-        float interpolationRatio = Mathf.Min(m_InterpolationSpeed * Time.deltaTime, 1);
+        float interpolationRatio = Mathf.Min(InterpolationSpeed * Time.deltaTime, 1);
         //current player direction
-        var playerDirection = m_Player.transform.position - transform.position;
+        var playerDirection = Player.transform.position - transform.position;
         //goal player direction
         playerDirection.y = 0;
-        playerDirection = (playerDirection.normalized) * m_MinDistance;
-        var goalPlayerDirection = playerDirection - m_OffsetY;
+        playerDirection = (playerDirection.normalized) * MinDistance;
+        var goalPlayerDirection = playerDirection - Offset;
         //goal rotation
         var goalRotation = Quaternion.LookRotation(goalPlayerDirection, Vector3.up);
         //goal position
-        var goalPosition = m_Player.transform.position - goalPlayerDirection;
+        var goalPosition = Player.transform.position - goalPlayerDirection;
 
         // Here we use linear interpolation for smooth camera movement
-        transform.position = m_EnableSmooth ? Vector3.Lerp(transform.position, goalPosition, interpolationRatio) : goalPosition;
-        transform.rotation = m_EnableSmooth ? Quaternion.Lerp(transform.rotation, goalRotation, interpolationRatio) : goalRotation;
+        transform.position = EnableSmooth ? Vector3.Lerp(transform.position, goalPosition, interpolationRatio) : goalPosition;
+        transform.rotation = EnableSmooth ? Quaternion.Lerp(transform.rotation, goalRotation, interpolationRatio) : goalRotation;
     }
 
 }
